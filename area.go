6cab1765fee9bcae35b6e02c246f60c2e84d6a32
Victor Navarro <victor.matias.navarro@gmail.com>
2020-08-10 15:13:12 -0300

    Create public area file

---
+++ area.go
@@ -0,0 +1,16 @@
+package area
+
+import "math"
+
+// Numeric proportion between circle and diameter
+const Pi = 3.1416
+
+// Circ area
+func Circ(r float64) float64 {
+	return math.Pow(r, 2) * Pi
+}
+
+//  Rect area
+func Rect(b, h float64) float64 {
+	return b * h;
+}
